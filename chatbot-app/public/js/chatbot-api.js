5da45d7f4b33c57268a9ea09f2f2f0e21b809755
Ajioh <panasonic745698@gmail.com>
2020-08-25 17:43:17 +0700

    asd

---
+++ chatbot-app/public/js/chatbot-api.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const url = 'https://dialogflow.clients6.google.com/v2/projects/ajioh-qkhb/agent/sessions/96d568b4-68e4-9e84-1c98-28d0a4dcfa05:detectIntent';
+const headers = {
+    'Content-type': 'Content-Type: application/json; charset=utf-8',
+    'Authorization': 'Bearer ya29.c.Ko8B2Qf6OBeiOR8wBAbApNA3GlAqC1o4zMjCmjLDk1dKeFTaCQ4C9_FzCm6loqWevChvLQeTBuJ2eosUGgQxhgQ8iZPo0O6M62XB95U4IWndpnJBFreWN40RQfP-CQNWGiCu-hejjJpj9ik2LD0v3AnCUPYRKxz1E1t0x9ig0oursW09nByMwDS2xTxDmDGvEQ4'
+};
+/**
+ * Get answer from message
+ * 
+ * @param {string} message message to chat
+ * 
+ * @return {Promise<any>} answer
+ */
+export async function chatbot(message) {
+    const payload = {
+        queryInput: {
+            text: {
+                text: message,
+                languageCode: 'ths',
+            },
+        },
+        queryParams: {
+            timezone: 'Asia/Bangkok';
+        }
+    }
+}
