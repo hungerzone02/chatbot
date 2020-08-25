ec7d04a4c79256f3d68be920b9b370434af00792
Ajioh <panasonic745698@gmail.com>
2020-08-25 18:22:02 +0700

    asd

--- chatbot-app/public/js/chatbot-api.js
+++ chatbot-app/public/js/chatbot-api.js
@@ -5,7 +5,7 @@ import * as apiClient from './api-client-fetch.js';
 const url = 'https://dialogflow.clients6.google.com/v2/projects/ajioh-qkhb/agent/sessions/96d568b4-68e4-9e84-1c98-28d0a4dcfa05:detectIntent';
 const headers = {
     'Content-type': 'application/json; charset=utf-8',
-    'Authorization': 'Bearer ya29.c.Ko8B2Qf6OBeiOR8wBAbApNA3GlAqC1o4zMjCmjLDk1dKeFTaCQ4C9_FzCm6loqWevChvLQeTBuJ2eosUGgQxhgQ8iZPo0O6M62XB95U4IWndpnJBFreWN40RQfP-CQNWGiCu-hejjJpj9ik2LD0v3AnCUPYRKxz1E1t0x9ig0oursW09nByMwDS2xTxDmDGvEQ4'
+    'Authorization': 'Bearer ya29.c.Ko8B2QcPlCX_ll4mJhh45YnQ6qh_Lzp2hF74tmFGlOLpZT1aMRbd7tsgwRCAoS_W018VWEdo1NotXMv4RJY6mc6EQt7dyotMa6lSwuko4MWRKOCAStVFn6BG678BICZgGjBEXH-R2GtQL9ZfDEmbg7SiTJhDdbzg04YgTU6zyqBPt_lIaFvfKmnipQFNyAIpgSU'
 };
 /**
  * Get answer from message
@@ -28,5 +28,11 @@ export async function chatbot(message) {
     };
 
     const res = await apiClient.post(url, payload, headers);
+
+    switch(res.queryResult.action) {
+        case 'input.ask.time':
+            return (new Date()).toLocaleTimeString()
+    }
+
     return res.queryResult.fulfillmentText;
 }
